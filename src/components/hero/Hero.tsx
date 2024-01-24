94b0f0f97a4f9cd59c2f4fba0a6184597bc997ff
fhilola <hilolafayoziddinova14@gmail.com>
2024-01-25 02:59:57 +0500

    hero section is done

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import Container from '../../utils/Utils'
+import { Link } from 'react-router-dom'
+import hero from '../../assets/hero.jpg'
+import './Hero.scss'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <Container>
+      <div className="hero">
+        <nav className='hero-navbar'>
+          <h1 className="hero-category">Men</h1>
+                <ul className="hero-list">
+                    <li className="hero-list-item">
+                        <Link to="/">Shoes</Link>
+                    </li>
+                    <li className="hero-list-item">
+                        <Link to="/">Clothing</Link>
+                    </li>
+                    <li className="hero-list-item">
+                        <Link to="/">Accessories</Link>
+                    </li>
+                    <li className="hero-list-item">
+                        <Link to="/">Sale</Link>
+                    </li>
+                </ul>
+        </nav>
+        <Link to="/">
+             <img className="hero__image" src={hero} alt="" />
+        </Link>
+        <div className="hero__content">
+                <h3>THE RESOLUTIONS STARTER PACK</h3>
+                <p>Get after your fitness goals with a curated collection of comfy kicks, lightweight tops, and more supportive styles.</p>
+                <Link className="link" to="/">Shop</Link>
+        </div>
+      </div>
+    </Container>
   )
 }
 
